7576ad2213290b52b2bb33072ccb1c44228b96e2
Akn <sibata486@gmail.com>
2024-12-01 17:14:06 +0900

    init /

---
+++ GpuMonitor/ViewModels/MainWindowViewModel.cs
@@ -0,0 +1,11 @@
+﻿using GpuMonitor.Models;
+using Prism.Mvvm;
+
+namespace GpuMonitor.ViewModels
+{
+    // ReSharper disable once ClassNeverInstantiated.Global
+    public class MainWindowViewModel : BindableBase
+    {
+        public TextWrapper TitleTextWrapper { get; set; } = new ();
+    }
+}
