67e8267dd50dbfe93cd53d8db5f763ab2010629a
Akn <sibata486@gmail.com>
2024-12-03 12:47:41 +0900

    add feat / 過剰な使用率の検出機能を AddUsage に追加した。

--- GpuMonitor/Models/UsageLogger.cs
+++ GpuMonitor/Models/UsageLogger.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections.ObjectModel;
 using Prism.Mvvm;
 
@@ -11,6 +12,9 @@ namespace GpuMonitor.Models
         private readonly ObservableCollection<int> usages = new ();
         private int latestUsage;
         private int logCapacity = 60;
+        private int usageExcessiveCounter;
+        private bool isWarningEnabled;
+        private bool isExcessive;
 
         public UsageLogger()
         {
@@ -30,11 +34,30 @@ namespace GpuMonitor.Models
         public int LogCapacity { get => logCapacity; set => SetProperty(ref logCapacity, value); }
 
         /// <summary>
-        /// このクラスが保持するリストの先頭に、引数で入力した値を挿入。要素数が `LogCapacity` を超えている場合は、末尾の要素を削除します。
+        /// 過剰な使用率が一定時間続いた時、警告を出すかどうかを設定・取得します。
         /// </summary>
-        /// <param name="usage">対象のリストの先頭に挿入する値。</param>
+        public bool IsWarningEnabled { get => isWarningEnabled; set => SetProperty(ref isWarningEnabled, value); }
+
+        /// <summary>
+        /// 使用率が過剰だと判定された時、true になります。
+        /// </summary>
+        public bool IsExcessive { get => isExcessive; private set => SetProperty(ref isExcessive, value); }
+
+        /// <summary>
+        /// このクラスが保持するリストの先頭に、引数で入力した値を挿入します。
+        /// </summary>
+        /// <remarks>
+        /// 要素数が `LogCapacity` を超えている場合は、末尾の要素を削除します。<br/>
+        /// また、usage が負の数の場合は処理を打ち切ります。
+        /// </remarks>
+        /// <param name="usage">リストの先頭に挿入する値。</param>
         public void AddUsage(int usage)
         {
+            if (usage < 0)
+            {
+                return;
+            }
+
             usages.Insert(0, usage);
             if (usages.Count > LogCapacity)
             {
@@ -42,6 +65,20 @@ namespace GpuMonitor.Models
             }
 
             LatestUsage = usage;
+            if (usage >= 80)
+            {
+                usageExcessiveCounter++;
+            }
+            else
+            {
+                usageExcessiveCounter = Math.Max(usageExcessiveCounter - 1, 0);
+            }
+
+            IsExcessive = usageExcessiveCounter >= 5;
+            if (IsExcessive && IsWarningEnabled)
+            {
+                // 警告を出力する
+            }
         }
     }
 }
